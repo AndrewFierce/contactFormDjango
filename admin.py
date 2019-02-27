2bdbcc4bbafb539d5c02c3a2119a98df13345e7b
AndrewFierce <37835573+AndrewFierce@users.noreply.github.com>
2019-02-27 09:50:23 +0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Subscribers
 
+# Представление списка e-mail пользователей для обратной связи
 class SubscribersAdmin (admin.ModelAdmin):
 	list_display = ('email', 'confirmation', 'randomkey')
 
