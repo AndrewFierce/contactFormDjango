6cd6dea1ad59dbfdff443425bc07ef4a57cdfdaa
AndrewFierce <37835573+AndrewFierce@users.noreply.github.com>
2019-02-27 09:48:59 +0300

    Update models.py

--- models.py
+++ models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 class Subscribers(models.Model):
     """
-    Model representing a Post.
+    Модель пользователей обратной связи.
     """
+    # e-mail пользователя
     email = models.EmailField(max_length=254, blank=True, default="")
+    # Подтвержден ли почтовый ящик или нет
     confirmation = models.BooleanField(default=False)
+    # Ключ подтверждения, который формируется в ссылке, отправленной на почту пользователя для подтверждения
     randomkey = models.CharField(max_length=100, help_text="Рандомный ключ")
     def __str__(self):
         """
